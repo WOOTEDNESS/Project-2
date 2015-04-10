55a9e8af24be10cf9eefe5c506fcfb348667aad6
Kevin <hemstrkj@clarkson.edu>
2015-04-10 00:43:39 -0400

    clean up only needed 2 folders
    if you want to examples fiels and such just ask.

--- project2.js
+++ project2.js
@@ -1,27 +1,40 @@
-var gl;
-var points;
+//
+//Cannon.js Variables
+//
+
+
+//
+//Three.js Variables
+//
+var scene, camera, renderer;
+
+
 window.onload = function init()
 {
-var canvas = document.getElementById( "gl-canvas" );
-gl = WebGLUtils.setupWebGL( canvas );
-if ( !gl ) { alert( "WebGL isn't available" ); }
-//
-// Configure WebGL
-//
-gl.viewport( 0, 0, canvas.width, canvas.height );
-gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-// Load shaders and initialize attribute buffers
-var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-gl.useProgram( program );
-render();
-};
-function render() {
-gl.clear( gl.COLOR_BUFFER_BIT );
-}
+    scene = new THREE.scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000);
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
+    //mak a simple cube
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
 
-    Status
-    API
-    Training
-    Shop
-    Blog
-    About
+
+	function render()
+	{
+		requestAnimationFrame(render);
+		cube.rotation.x += .1;
+		cube.rotation.y += .1;
+		renderer.render(scene,camera);
+	}
+
+	
+	render();
+
+};
+
