400388b431c9afc1c51feb3b9afca168e3aecc66
Kevin John Hemstreet-Grimmer <kjhg345@gmail.com>
2015-04-15 20:01:48 -0400

    redo some shit

--- project2.js
+++ project2.js
@@ -1,27 +1,44 @@
-var gl;
-var points;
+//
+//Three.js
+//
+
+//
+//cannon.js
+//
+
+
 window.onload = function init()
 {
-var canvas = document.getElementById( "gl-canvas" );
-gl = WebGLUtils.setupWebGL( canvas );
-if ( !gl ) { alert( "WebGL isn't available" ); }
-//
-// Configure WebGL
-//
-gl.viewport( 0, 0, canvas.width, canvas.height );
-gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-// Load shaders and initialize attribute buffers
-var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-gl.useProgram( program );
-render();
-};
-function render() {
-gl.clear( gl.COLOR_BUFFER_BIT );
+            var scene = new THREE.Scene();
+            var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+            var renderer = new THREE.WebGLRenderer();
+            renderer.setSize( window.innerWidth, window.innerHeight );
+            document.body.appendChild( renderer.domElement );
+
+            var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+            var material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
+            var cube = new THREE.Mesh( geometry, material );
+            scene.add( cube );
+
+            camera.position.z = 10;
+            //camera.position. = 3;
+
+            render();
+
+
+
 }
 
-    Status
-    API
-    Training
-    Shop
-    Blog
-    About
+function render() 
+{
+
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    requestAnimationFrame( render );
+
+                cube.rotation.x += 0.01;
+                cube.rotation.y += 0.01;
+
+                renderer.render(scene, camera);
+    
+}
