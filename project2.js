a0a6e9b4b7c8108f45b0393917eaf6634be4aaf8
Kevin <hemstrkj@clarkson.edu>
2015-04-16 22:14:48 -0400

    added fucntions to do

--- project2.js
+++ project2.js
@@ -9,24 +9,24 @@ var geometry,material,cube, pyramid;
 //
 
 
+var flag;
+
 window.onload = function init()
 {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    initScene();
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-
-            geometry = new THREE.BoxGeometry( 1, 1, 1 );
-            material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
-            cube = new THREE.Mesh( geometry, material );
-            scene.add( cube );
-
-            camera.position.z = 5;
+    initCannon();
 
 
-            document.onclick = function(event){AddShape(event);};
+
+    camera.position.z = 5;
+
+
+            document.onclick = function (event) { AddShape(event); };
+
+            document.getElementById("pryamids").onclick = function (event) { ChangeToTriangle(event); };
+            document.getElementById("sphere").onclick = function (event) { ChangeToSphere(event); };
+            document.getElementById("cubes").onclick = function (event) { ChangeToCube(event); };
             
 
             render();
@@ -35,26 +35,90 @@ window.onload = function init()
 
 }
 
-function render() 
-{
-    requestAnimationFrame( render );
-
-                cube.rotation.x += 0.01;
-                cube.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
-    
-}
-
 function AddShape(event)
 {
-    //onclick add a shape to a specified default
-    //TODO: at location of mouse
-    geometry = new THREE.BoxGeometry( 3, 3, 3 );
-    material = new THREE.MeshBasicMaterial( { color: 0x00ff } );
-    cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    //TODO: depending what button was last pressed via flag and switch 
+    switch(flag)
+    {
+        case"s":
+            break;
+        case"q":
+            break;
+        case"p":
+            break;
+    }
+  
+
 }
+
+
+
+function render() 
+{
+    //TODO: add all world stuff to scene 
+    //and some camera ops (ie mouse sweep, onMouseDown, only x and y)
+    requestAnimationFrame( render );
+
+    renderer.render(scene, camera);
+}
+
+
+function initScene()
+{
+    //TODO: initalize one shape in the center, maybe have it fall? 
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    //addds first cube to scene
+    geometry = new THREE.BoxGeometry(1, 1, 1);
+    material = new THREE.MeshBasicMaterial({ color: 0x00ffff });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+}
+function initCannon()
+{
+    //TODO: hit all the physics shit
+
+}
+
+function ClearToPyramid(event)
+{
+    //TODO: clear scene then add triagnles
+
+}
+
+function AddPyramid()
+{
+    //adds to exits scene
+}
+
+function ClearToSphere(event)
+{
+    //TODO: clear scene then add shperes
+
+}
+
+function AddPyramid()
+{
+    //adds to exits scene
+}
+
+function ClearToCube(event)
+{
+    //TODO: clear scene then add cubes to (x,y) of mouce click
+    geometry = new THREE.BoxGeometry(3, 3, 3);
+    material = new THREE.MeshBasicMaterial({ color: 0x00ff });
+    cube = new THREE.Mesh(geometry, material);
+    //change posisiton 
+    scene.add(cube);
+
+}
+
 
 
 
